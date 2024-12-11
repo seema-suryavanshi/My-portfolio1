e35706856f56ed380feadca5bc73d7233df64e4c
seema-suryavanshi <seema12362@gmail.com>
2024-12-12 02:21:37 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+// Simple contact form submission
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form from refreshing the page
+    alert('Thank you for reaching out! I will get back to you soon.');
+  });
+  
